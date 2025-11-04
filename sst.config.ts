23a0ff6ab5565817d213b450b3bc613a5699bf23
Aidan Lowson <dev@aidanlowson.com>
2025-11-04 17:11:57 +0000

    chore: update astro-sst version to 3.1.4 and refine build commands in configuration

--- sst.config.ts
+++ sst.config.ts
@@ -4,18 +4,15 @@ export default $config({
   app(input) {
     return {
       name: 'dev-daily-hub',
-      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      removal: 'remove', // input?.stage === 'production' ? 'retain' : 'remove',
       home: 'aws',
       region: 'us-east-1',
     }
   },
   async run() {
-    // SST disallows top-level imports in sst.config.ts. Do a dynamic import
-    // for values we need from `@consts` so the config remains valid.
-    const { domainAlias, domainName } = await import('@consts')
-
     new sst.aws.Astro('dev_daily_hub', {
-      domain: { name: domainName, redirects: [domainAlias] },
+      domain: { name: 'devdailyhub.com', redirects: ['www.devdailyhub.com'] },
+      buildCommand: 'astro check --minimumSeverity warning && astro build',
     })
   },
 })
