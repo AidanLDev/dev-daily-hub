e83b8b2dba23ca6966518fc79b91de44225cf4f1
Aidan Lowson <dev@aidanlowson.com>
2025-11-23 17:56:33 +0000

    Adding sentry tracing

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,8 +1,9 @@
 import * as Sentry from '@sentry/astro'
 
 Sentry.init({
-  dsn: 'https://fb12ebc0acf269cd93a5f4861cfd6dd2@o4510397968154624.ingest.de.sentry.io/4510398283317328',
+  dsn: process.env.SENTRY_DNS,
   // Adds request headers and IP for users, for more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/astro/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
+  tracesSampleRate: 1.0,
 })
