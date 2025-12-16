c5ea21df4d1540ef5b95bca7ed1ba58606b7ec7c
AidanLDev <dev@aidanlowson.com>
2025-12-16 16:39:21 +0000

    Update dependencies and fix compatibility issues
    - Updated `tokio` to version 1 with features enabled.
    - Updated `lambda_runtime` and `lambda_http` to version 1.0.1.
    - Addressed compatibility issues with Rust compiler version by noting required versions for dependencies in the error log.
    - Added new binary files and metadata for various dependencies in the debug target.
    - Generated new timestamp files for invoked builds and updated dependency graphs.

---
+++ target/debug/build/proc-macro2-eec3f5b46e2890fd/build_script_build-eec3f5b46e2890fd.d
@@ -0,0 +1,5 @@
+C:\Users\Aidan\Repos\dev-daily-hub\target\debug\build\proc-macro2-eec3f5b46e2890fd\build_script_build-eec3f5b46e2890fd.exe: C:\Users\Aidan\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.103\build.rs
+
+C:\Users\Aidan\Repos\dev-daily-hub\target\debug\build\proc-macro2-eec3f5b46e2890fd\build_script_build-eec3f5b46e2890fd.d: C:\Users\Aidan\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.103\build.rs
+
+C:\Users\Aidan\.cargo\registry\src\index.crates.io-6f17d22bba15001f\proc-macro2-1.0.103\build.rs:
---
+++ target/debug/deps/pin_project_lite-5a7e24f33556c1f3.d
@@ -0,0 +1,5 @@
+C:\Users\Aidan\Repos\dev-daily-hub\target\debug\deps\libpin_project_lite-5a7e24f33556c1f3.rmeta: C:\Users\Aidan\.cargo\registry\src\index.crates.io-6f17d22bba15001f\pin-project-lite-0.2.16\src/lib.rs
+
+C:\Users\Aidan\Repos\dev-daily-hub\target\debug\deps\pin_project_lite-5a7e24f33556c1f3.d: C:\Users\Aidan\.cargo\registry\src\index.crates.io-6f17d22bba15001f\pin-project-lite-0.2.16\src/lib.rs
+
+C:\Users\Aidan\.cargo\registry\src\index.crates.io-6f17d22bba15001f\pin-project-lite-0.2.16\src/lib.rs:
