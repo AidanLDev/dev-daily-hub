36f26fbb0a52987c3862db2f5fe0217c29995e15
Aidan Lowson <dev@aidanlowson.com>
2025-12-22 16:05:20 +0000

    Add about page and update navigation links

--- dev-daily-hub-lambda/src/handlers.rs
+++ dev-daily-hub-lambda/src/handlers.rs
@@ -3,7 +3,7 @@ use lambda_http::{Response, Body};
 
 #[derive(Template)]
 #[template(path = "index.html")]
-pub struct IndexTemplate<'a> { pub name: &'a str }
+pub struct IndexTemplate<'a> { pub name: &'a str, }
 
 pub async fn index() -> Response<Body> {
   let template = IndexTemplate { name: "Dev Daily Hub" };
@@ -17,6 +17,19 @@ pub async fn index() -> Response<Body> {
 #[derive(Template)]
 #[template(path = "fragment.html")]
 struct FragmentTemplate { pub message: &'static str }
+
+#[derive(Template)]
+#[template(path = "about.html")]
+struct AboutTemplate { pub image_base_url: String }
+
+pub async fn about() -> Response<Body> {
+  let template = AboutTemplate { image_base_url: String::from("/images/") };
+  Response::builder()
+    .header("content-type", "text/html; charset=utf-8")
+    .status(200)
+    .body(Body::Text(template.render().unwrap()))
+    .unwrap()
+}
 
 pub async fn fragment() -> Response<Body> {
     let f = FragmentTemplate { message: "HTMX loaded content from server" };
--- dev-daily-hub-lambda/src/main.rs
+++ dev-daily-hub-lambda/src/main.rs
@@ -4,9 +4,6 @@ use lambda_runtime::Error;
 use mime_guess::from_path;
 use tokio::fs;
 
-
-
-
 async fn serve_static(path: &str) -> Result<Response<Body>, Error> {
     let filepath = if path.starts_with("/static/") {
         format!(".{}", path) // ./static/...
@@ -31,6 +28,7 @@ async fn handler(req: Request) -> Result<Response<Body>, Error> {
     match path.as_str() {
         "/" => Ok(handlers::index().await),
         "/fragment" => Ok(handlers::fragment().await),
+        "/about" => Ok(handlers::about().await),
         p if p.starts_with("/static/") || p.starts_with("/images/") => serve_static(&p).await,
         _ => Ok(Response::builder().status(404).body(Body::Text("Not found".into())).unwrap()),
     }
@@ -38,8 +36,6 @@ async fn handler(req: Request) -> Result<Response<Body>, Error> {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // Use the lambda_http run function which accepts an HTTP service_fn
-    // and returns the appropriate Result<(), Error> for this crate.
     lambda_http::run(service_fn(handler)).await
 }
 
